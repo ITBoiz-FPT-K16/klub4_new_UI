3c15002f003b70fca5600d1bf12806ce2b7ee67e
giabao <baotran2115@gmail.com>
2023-07-18 14:31:58 +0700

    Pay Fund Component

--- frontend/src/components/fund/index.js
+++ frontend/src/components/fund/index.js
@@ -1,46 +1,75 @@
+import * as React from "react";
 import { useState } from "react";
+import AddCardIcon from "@mui/icons-material/AddCard";
+import QRCode from "react-qr-code";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
+import { QrCode } from "@mui/icons-material";
+
 const Funds = ({ fund, user }) => {
   const [visible, setVisible] = useState(false);
+  const [open, setOpen] = React.useState(false);
+  const style = {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: 400,
+    bgcolor: "background.paper",
+    border: "2px solid #000",
+    boxShadow: 24,
+    p: 4,
+  };
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
   const [showMenu, setShowMenu] = useState(false);
   return (
     <div className="post">
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Scan QR CODE TO PAY
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            <QRCode
+              size={100}
+              bgcolor="white"
+              fgcolor="black"
+              value="This is a value"
+            />
+          </Typography>
+        </Box>
+      </Modal>
       <div className="post_header">
         <div
           className="fund_header_right hover1"
           onClick={() => setShowMenu(!showMenu)}
         >
-          <div className="post_bg" style={{}}>
-            <div className="post_bg_text">{fund.name}</div>
-          </div>
-          <div className="post_text">{fund.description}</div>
           <div className="post_text">
-            <strong> Fund to pay: {fund.price}</strong>
+            {fund.name} <br />
+            {fund.description}
           </div>
-        </div>
-      </div>
-
-      <div className="fund_actions">
-        <div
-          className="fund_action hover1"
-          onMouseOver={() => {
-            setTimeout(() => {
-              setVisible(true);
-            }, 500);
-          }}
-          onMouseLeave={() => {
-            setTimeout(() => {
-              setVisible(false);
-            }, 500);
-          }}
-        >
-          <div className=" middle_icon hover1">
-            <div className="mr-2">
-              <i className="share_icon"> </i> Pay Fund
-            </div>
+          <div className="post_text">
+            <strong> Fund to pay: ${fund.price}</strong>
+          </div>
+          <div className="ml-3">
+            <button class="button-4">
+              <AddCardIcon />
+              <Button onClick={handleOpen}>Pay Now!</Button>
+            </button>
           </div>
         </div>
       </div>
     </div>
   );
 };
+
 export default Funds;
